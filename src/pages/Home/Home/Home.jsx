71d1cbcbe0c7a16f6461481efaf12284dea3e7f8
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2023-11-14 09:02:13 +0600

    Testimonials section added in home page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import FeaturedItem from "../FeaturedItem/FeaturedItem";
 import OrderOnline from "../OrderOnline/OrderOnline";
 import PopularItems from "../PopularItems/PopularItems";
 import Slider from "../Slider/Slider";
+import Testimonials from "../Testimonials/Testimonials";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <OrderOnline />
       <PopularItems />
       <FeaturedItem />
+      <Testimonials />
     </>
   );
 };
---
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,60 @@
+import SectionContainer from "../../Shared/SectionContainer/SectionContainer";
+import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
+import { useEffect, useState } from "react";
+import { FaQuoteLeft } from "react-icons/fa6";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import ReactStars from "react-stars";
+
+const Testimonials = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("/reviews.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setReviews(data);
+      });
+  }, []);
+  return (
+    <div className="py-12 md:py-20">
+      <SectionContainer>
+        <SectionTitle
+          subtitle={"What Our Clients Say"}
+          title={"Testimonials"}
+        />
+        <div>
+          <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+            {reviews.length > 0 &&
+              reviews.map((review) => (
+                <SwiperSlide key={review._id}>
+                  <div className="text-center mt-5 md:mt-10">
+                    <ReactStars
+                      className="mx-auto w-max"
+                      count={5}
+                      value={review.rating}
+                      edit={false}
+                      size={32}
+                      color2={"#ffbb00"}
+                    />
+                    <FaQuoteLeft className="text-6xl sm:text-7xl mx-auto pt-2" />
+                    <p className="text-sm sm:text-base px-10 sm:px-12 md:px-16 pt-5">
+                      {review.details}
+                    </p>
+                    <h3 className="text-xl sm:text-2xl font-medium text-yellow-600 pt-2">
+                      {review.name}
+                    </h3>
+                  </div>
+                </SwiperSlide>
+              ))}
+          </Swiper>
+        </div>
+      </SectionContainer>
+    </div>
+  );
+};
+
+export default Testimonials;
