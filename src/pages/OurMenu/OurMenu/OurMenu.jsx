6e0f24e99fda5500ea5807a87d77ceb0939791d2
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2023-11-15 11:19:38 +0600

    Todays offer added in our menu page

--- src/pages/OurMenu/OurMenu/OurMenu.jsx
+++ src/pages/OurMenu/OurMenu/OurMenu.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import PageCover from "../../Shared/PageCover/PageCover";
 import menuImg from "../../../assets/menu/banner3.jpg";
+import TodaysOffer from "../TodaysOffer/TodaysOffer";
 
 const OurMenu = () => {
   return (
@@ -13,6 +14,7 @@ const OurMenu = () => {
         title={"Our menu"}
         subtitle={"Would you like to try our dish?"}
       />
+      <TodaysOffer />
     </>
   );
 };
---
+++ src/pages/OurMenu/TodaysOffer/TodaysOffer.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
+import SectionContainer from "../../Shared/SectionContainer/SectionContainer";
+
+const TodaysOffer = () => {
+  const [popularItems, setPopularItems] = useState([]);
+  useEffect(() => {
+    fetch("/menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const popular = data.filter((item) => item.category === "offered");
+        setPopularItems(popular);
+      });
+  }, []);
+
+  return (
+    <div className="py-10 md:py-14">
+      <SectionContainer>
+        <SectionTitle subtitle={"Don't miss"} title={"Today's offer"} />
+        {popularItems?.length > 0 ? (
+          <div className="grid lg:grid-cols-2 gap-5 pt-12">
+            {popularItems.map((item) => (
+              <MenuItem key={item._id} item={item} />
+            ))}
+          </div>
+        ) : (
+          <div className="text-center text-xl sm:text-4xl font-semibold py-14">
+            No Item Found
+          </div>
+        )}
+        <div className="text-center pt-8 sm:pt-12">
+          <button className="uppercase sm:text-lg font-medium border-b-2 border-black px-5 py-2 rounded-md bg-slate-100 hover:bg-slate-400">
+            Order your favourite food
+          </button>
+        </div>
+      </SectionContainer>
+    </div>
+  );
+};
+
+export default TodaysOffer;
